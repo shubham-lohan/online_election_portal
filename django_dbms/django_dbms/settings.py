7589c8d71beb4aa55ec99ce56e000460b2e3fb98
Bassam Pervez <bassampervez@gmail.com>
2021-03-18 22:25:23 +0530

    test

--- django_dbms/django_dbms/settings.py
+++ django_dbms/django_dbms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't1cct-kwpsns0i^%-f-@veq3%#ag)1w5us00jm9cls7x*9!r+4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['election-dbms.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0','election-dbms.herokuapp.com','127.0.0.1']
 
 
 # Application definition
