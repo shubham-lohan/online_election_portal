6c6c4af3467472299c43367320edd291ac34d954
Bassam Pervez <bassampervez@gmail.com>
2021-03-21 01:42:44 +0530

    dict

--- django_dbms/election/urls.py
+++ django_dbms/election/urls.py
@@ -6,7 +6,7 @@ from .views import home, login, cand_profile, register_candidate, register_party
 urlpatterns = [
     path('', home, name='home'),
     path(r'login', login, name='login'),
-    path('register', register_voter, name="register_voter"),
+    path('register', register_voter, name='register'),
     path('profile/candidate', cand_profile, name='cand_profile'),
     path('register/candidate', register_candidate, name="reg_cand"),
     path('register/party', register_party, name="register_party"),
