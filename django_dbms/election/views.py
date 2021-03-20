f7e8d7d2e626ac29cce26c1c3d808c393a86112a
Bassam Pervez <bassampervez@gmail.com>
2021-03-21 02:59:53 +0530

    t

--- django_dbms/election/views.py
+++ django_dbms/election/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User,authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.http import HttpResponse
 
 # Create your views here.
