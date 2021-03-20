43bba109a81a7d6b50e4170de7b70dc255824386
Bassam Pervez <bassampervez@gmail.com>
2021-03-21 00:02:36 +0530

    test

--- django_dbms/election/urls.py
+++ django_dbms/election/urls.py
@@ -6,11 +6,10 @@ from .views import home, login, cand_profile, register_candidate, register_party
 urlpatterns = [
     path('', home, name='home'),
     path(r'login', login, name='login'),
+    path('register', register_voter, name="register_voter"),
     path('profile/candidate', cand_profile, name='cand_profile'),
-    path(r'register', register, name="register"),  # problem in adding this!
     path('register/candidate', register_candidate, name="reg_cand"),
     path('register/party', register_party, name="register_party"),
-    path('register/voter', register_voter, name="register_voter"),
     path('register/official', register_official, name="register_official"),
 
 ]
