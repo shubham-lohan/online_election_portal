c3d791adbd9502d40a1a0aa87d1130a18502a365
Bassam Pervez <bassampervez@gmail.com>
2021-03-20 13:30:42 +0530

    fixed register

--- django_dbms/staticfiles/js/register.js
+++ django_dbms/staticfiles/js/register.js
@@ -4,23 +4,23 @@ function SelectRedirect() {
 
     switch (document.getElementById('s1').value) {
         case "voter":
-            window.location = "{%static 'register_voter'%}";
+            window.location = "{% url 'register_voter' %}";
             break;
 
         case "candidate":
-            window.location = "{%static 'register_candidate'%}";
+            window.location = "{% url 'reg_cand'%}";
             break;
 
         case "party":
-            window.location = "{%static 'register_party'%}";
+            window.location = "{% url 'register_party'%}";
             break;
         case "official":
-            window.location = "{%static 'register_official'%}";
+            window.location = "{% url 'register_official'%}";
             break;
 
         /// Can be extended to other different selections of SubCategory //////
         default:
-            window.location = "{%static 'home'%}"; // if no selection matches then redirected to home page
+            # window.location = "{% url 'home'%}"; // if no selection matches then redirected to home page
             break;
     }// end of switch 
 }
