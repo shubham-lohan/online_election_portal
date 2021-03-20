afe554542345915ec4c15b8d00f9bbae492182ef
Bassam Pervez <bassampervez@gmail.com>
2021-03-21 03:39:36 +0530

    f

--- django_dbms/django_dbms/settings.py
+++ django_dbms/django_dbms/settings.py
@@ -87,8 +87,10 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-AUTH_USER_MODEL = 'django_dbms.User'
+AUTH_USER_MODEL = 'django.contrib.auth.models.User'
+
 AUTH_PASSWORD_VALIDATORS = [
+
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
