0f0c479f6089a79c8c9bb59b0004afb8abc1fbc6
Bassam Pervez <bassampervez@gmail.com>
2021-03-20 13:30:42 +0530

    fixed register

--- django_dbms/election/urls.py
+++ django_dbms/election/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path
+from django.urls import path,re_path
 from django.conf.urls import url, include
 
 from .views import home, login, cand_profile, register_candidate, register_party, register_voter, register_official, register
 
 urlpatterns = [
     path('', home, name='home'),
-    path('login', login, name='login'),
+    path(r'login', login, name='login'),
     path('profile/candidate', cand_profile, name='cand_profile'),
-    # path('register', register, name="register"),  # problem in adding this!
-    path('regsiter/candidate', register_candidate, name="reg_cand"),
-    path('regsiter/party', register_party, name="register_party"),
-    path('regsiter/voter', register_voter, name="register_voter"),
-    path('regsiter/official', register_official, name="register_official"),
+    path(r'register', register, name="register"),  # problem in adding this!
+    path('register/candidate', register_candidate, name="reg_cand"),
+    path('register/party', register_party, name="register_party"),
+    path('register/voter', register_voter, name="register_voter"),
+    path('register/official', register_official, name="register_official"),
 
 ]
