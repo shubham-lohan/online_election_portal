328bd4915402b3f5eb79a0a679676913c7e0bb5a
Kushal Juneja <kushal19057@iiitd.ac.in>
2021-04-09 22:05:20 +0530

    Create godfile.sql

---
+++ mysql_queries/godfile.sql
@@ -0,0 +1,1 @@
+# This file will contain everything. Hence the name GODFILE. Think of it link recreating the entire system on local host in a short duration. How will you do , by runnning this file.
