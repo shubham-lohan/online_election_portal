96133f9a3e7187957844568c71b8afecc4df3351
Kushal Juneja <kushal19057@iiitd.ac.in>
2021-04-15 18:17:52 +0530

    Delete godfile.sql

--- mysql_queries/godfile.sql
+++
@@ -1,1 +0,0 @@
-# This file will contain everything. Hence the name GODFILE. Think of it link recreating the entire system on local host in a short duration. How will you do , by runnning this file.
